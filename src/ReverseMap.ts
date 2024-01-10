563607c10fc4202532ce29d0c1df38e5a11946ee
Richard Ivanek <richard.ivanek@gmail.com>
2024-01-10 19:19:43 +0100

    docs: add documentation to ReverseMap<K, V> to mirror es2015 Map

--- src/ReverseMap.ts
+++ src/ReverseMap.ts
@@ -1,5 +1,16 @@
 export default interface ReverseMap<K, V> {
+  /**
+   * @returns a boolean indicating whether an element with the specified key existed and has been removed or not.
+   */
   deleteValue(value: V): boolean;
+
+  /**
+   * @return the key associated with a given value if the value is present in the map.
+   */
   getKey(value: V): K | undefined;
+
+  /**
+   * @returns a boolean indicating whether an element with the specified value exists or not.
+   */
   hasValue(value: V): boolean;
 }
